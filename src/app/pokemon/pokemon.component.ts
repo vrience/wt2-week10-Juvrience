652e9a8d292699ca45380e88dd9e62c10c3d4f9a
vrience <vriencevml@gmail.com>
2020-05-05 18:47:33 +0700

    di 1:05:22

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
 import { PokemonService } from "./pokemon.service";
+import { Observable } from "tns-core-modules/ui/page/page";
 
 @Component({
     selector: "ns-items",
     templateUrl: "./pokemon.component.html"
 })
 export class PokemonComponent implements OnInit {
+
     pokemons;
 
     constructor(private ps: PokemonService) { }
@@ -14,7 +16,6 @@ export class PokemonComponent implements OnInit {
     ngOnInit(): void {
         this.ps.getPokemon().subscribe((response) => {
                 this.pokemons = response.results;
-                // console.log(this.pokemons);
             });
     }
 }
